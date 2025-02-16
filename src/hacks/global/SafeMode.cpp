cb007ca6d9e040a4b399304875a8750330b68a4f
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2025-02-16 18:01:41 -0500

    Safe mode and half freeze attempts i think i havent tested

---
+++ src/hacks/global/SafeMode.cpp
@@ -0,0 +1,64 @@
+#include "Config.hpp"
+#include "Geode/binding/GameStatsManager.hpp"
+#include "ui/UIManager.hpp"
+#include "ui/widgets/ToggleWidget.hpp"
+#include <hacks/Hack.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+namespace summit::hack::global {
+  class SafeMode : Hack {
+    public:
+      std::string getTab() override { return "global"; }
+      std::string getId() override { return "global.safemode"; }
+      void init() override {
+        ui::UIManager::registerWidget(getId(), ui::widgets::ToggleWidget::create(getId(), "Safe Mode", Config::get<bool>("global.safemode", false))->setDescription("Prevents you from beating any levels.")->setCallback([](bool toggled) {
+          Config::set<bool>("global.safemode", toggled);
+        }));
+      }
+  };
+
+  // class AutoSafeMode : Hack {
+  //   public:
+  //     std::string getTab() override { return "global"; }
+  //     std::string getId() override { return "global.autosafemode"; }
+  //     void init() override {
+  //       ui::UIManager::registerWidget(getId(), ui::widgets::ToggleWidget::create(getId(), "Auto Safe Mode", false)->setDescription("Prevents you from beating any levels if you have any cheats enabled."));
+  //     }
+  // };
+
+  class $modify(SMPlayLayer, PlayLayer) {
+    struct Fields {
+      int m_jumps;
+      int m_attempts;
+    };
+
+    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+      auto man = GameStatsManager::get();
+      m_jumps = man->getStat("1");
+      m_attempts = man->getStat("2");
+      return PlayLayer::init(level, useReplay, dontCreateObjects);
+    }
+
+    void levelComplete() {
+      bool sm = summit::Config::get<bool>("global.safemode", true);
+      if (summit::Config::get<bool>("global.safemode", true)) {
+        auto orig = m_isTestMode;
+        m_isTestMode = true;
+        PlayLayer::levelComplete();
+        m_isTestMode = orig;
+      } else PlayLayer::levelComplete();
+    }
+
+    void destroyPlayer(PlayerObject *p, GameObject *g) {
+      if (summit::Config::get<bool>("global.safemode", true)) {
+        auto orig = m_isTestMode;
+        m_isTestMode = true;
+        PlayLayer::destroyPlayer(p,g);
+        m_isTestMode = orig;
+        auto man = GameStatsManager::get();
+        man->setStat("1", m_jumps);
+        man->setStat("2", m_attempts);
+      } else PlayLayer::destroyPlayer(p,g);
+    }
+  };
+}
--- src/internal/hacks/Hack.hpp
+++ src/internal/hacks/Hack.hpp
@@ -1,16 +1,15 @@
 #pragma once
 
+#include <string>
 namespace summit::hack {
 class Hack {
-  protected:
-    std::string id;
-    std::string tab;
-
   public:
     virtual std::string getId() = 0;
     virtual std::string getTab() = 0;
 
-    virtual void update(float delta) {};
+    virtual void update(float delta) {}
     virtual void init() = 0;
+
+    virtual bool isCheat() { return true; }
 };
 }
